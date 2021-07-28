d87dede356db21d5a2c25e322c03a56fbc08d857
Ronald Rajski <ronaldrajski@gmail.com>
2021-07-27 21:35:10 -0500

    HR3 conditionals

---
+++ src/HackerRankDay3/Solution.java
@@ -0,0 +1,38 @@
+package HackerRankDay3;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        String ans = "";
+
+        // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
+        if(n % 2 == 1) {
+            ans = "Weird";
+        }
+        else {
+            if (n >= 2 && n <= 5)
+                ans = "Not Weird";
+            else if (n >= 6 && n <= 20)
+                ans = "Weird";
+            else if (n > 20)
+                ans = "Not Weird";
+        }
+        System.out.println(ans);
+    }
+}
