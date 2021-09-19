b995b5596727b513b8086f34b733faf705e67e58
Ronald Rajski <ronaldrajski@gmail.com>
2021-09-19 17:50:37 -0500

    added wayToFly

--- src/PearsonChapter13/HW3/WayToFly.java
+++ src/PearsonChapter13/HW3/WayToFly.java
@@ -1,4 +1,35 @@
 package PearsonChapter13.HW3;
 
+
+//Movement for plane
 public interface WayToFly {
+
+    /********************************************
+     * Name: Ronald Rajski
+     * Class: COSC 1174
+     * Assignment: HW3 Vehicles
+     * Date: 9-17-21
+     ********************************************/
+
+//interface Way to drive
+    public interface WayToDrive {
+        //    describes direction you are turning
+        void turnLeft();
+
+        //    describes direction you are turning
+        void turnRight();
+
+        //describbes moving forward
+        void g0Straight();
+
+    //        describes going up
+
+        public void up();
+
+        //        describes going down
+        public void down();
+
+    }
+
+
 }
